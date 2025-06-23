e797b1d9d5ee0b905cca407f1e8b6d673f375d4e
sdcb <sdflysha@qq.com>
2025-06-23 18:21:47 +0800

    enable all v4 models

--- tests/Sdcb.PaddleOCR.Tests/OfflineModelsTest.cs
+++ tests/Sdcb.PaddleOCR.Tests/OfflineModelsTest.cs
@@ -29,19 +29,19 @@ public class OfflineModelsTest
         FastCheck(model);
     }
 
-    //[Fact]
-    //public void FastCheckOCRChineseV4()
-    //{
-    //    FullOcrModel model = LocalFullModels.ChineseV4;
-    //    FastCheck(model);
-    //}
+    [Fact]
+    public void FastCheckOCRChineseV4()
+    {
+        FullOcrModel model = LocalFullModels.ChineseV4;
+        FastCheck(model);
+    }
 
-    //[Fact]
-    //public void FastCheckOCRChineseV5()
-    //{
-    //    FullOcrModel model = LocalFullModels.ChineseV5;
-    //    FastCheck(model);
-    //}
+    [Fact]
+    public void FastCheckOCRChineseV5()
+    {
+        FullOcrModel model = LocalFullModels.ChineseV5;
+        FastCheck(model);
+    }
 
     private void FastCheck(FullOcrModel model)
     {
@@ -68,32 +68,32 @@ public class OfflineModelsTest
         }
     }
 
-    //[Fact]
-    //public async Task QueuedOCR()
-    //{
-    //    FullOcrModel model = LocalFullModels.EnglishV3;
+    [Fact]
+    public async Task QueuedOCR()
+    {
+        FullOcrModel model = LocalFullModels.EnglishV4;
 
-    //    // from: https://visualstudio.microsoft.com/wp-content/uploads/2021/11/Home-page-extension-visual-updated.png
-    //    byte[] sampleImageData = File.ReadAllBytes(@"./samples/vsext.png");
+        // from: https://visualstudio.microsoft.com/wp-content/uploads/2021/11/Home-page-extension-visual-updated.png
+        byte[] sampleImageData = File.ReadAllBytes(@"./samples/vsext.png");
 
-    //    using QueuedPaddleOcrAll all = new(() => new PaddleOcrAll(model)
-    //    {
-    //        AllowRotateDetection = true,
-    //        Enable180Classification = false,
-    //    }, consumerCount: 1, boundedCapacity: 64);
+        using QueuedPaddleOcrAll all = new(() => new PaddleOcrAll(model)
+        {
+            AllowRotateDetection = true,
+            Enable180Classification = false,
+        }, consumerCount: 1, boundedCapacity: 64);
 
-    //    {
-    //        // Load local file by following code:
-    //        // using (Mat src2 = Cv2.ImRead(@"C:\test.jpg"))
-    //        using (Mat src = Cv2.ImDecode(sampleImageData, ImreadModes.Color))
-    //        {
-    //            PaddleOcrResult result = await all.Run(src);
-    //            _console.WriteLine("Detected all texts: \n" + result.Text);
-    //            foreach (PaddleOcrResultRegion region in result.Regions)
-    //            {
-    //                _console.WriteLine($"Text: {region.Text}, Score: {region.Score}, RectCenter: {region.Rect.Center}, RectSize:    {region.Rect.Size}, Angle: {region.Rect.Angle}");
-    //            }
-    //        }
-    //    }
-    //}
+        {
+            // Load local file by following code:
+            // using (Mat src2 = Cv2.ImRead(@"C:\test.jpg"))
+            using (Mat src = Cv2.ImDecode(sampleImageData, ImreadModes.Color))
+            {
+                PaddleOcrResult result = await all.Run(src);
+                _console.WriteLine("Detected all texts: \n" + result.Text);
+                foreach (PaddleOcrResultRegion region in result.Regions)
+                {
+                    _console.WriteLine($"Text: {region.Text}, Score: {region.Score}, RectCenter: {region.Rect.Center}, RectSize:    {region.Rect.Size}, Angle: {region.Rect.Angle}");
+                }
+            }
+        }
+    }
 }
