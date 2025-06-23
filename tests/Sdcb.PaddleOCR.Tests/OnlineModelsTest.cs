0bdc233fbbe0a25f39e72810fbf79df06e785e62
sdcb <sdflysha@qq.com>
2025-06-23 18:32:52 +0800

    V4Mkldnn only

--- tests/Sdcb.PaddleOCR.Tests/OnlineModelsTest.cs
+++ tests/Sdcb.PaddleOCR.Tests/OnlineModelsTest.cs
@@ -8,44 +8,37 @@ using Xunit.Abstractions;
 
 namespace Sdcb.PaddleOCR.Tests;
 
-public class OnlineModelsTest
+public class OnlineModelsTest(ITestOutputHelper console)
 {
-    private readonly ITestOutputHelper _console;
+    //[Fact]
+    //public async Task FastCheckOCR()
+    //{
+    //    FullOcrModel model = await OnlineFullModels.EnglishV3.DownloadAsync();
 
-    public OnlineModelsTest(ITestOutputHelper console)
-    {
-        _console = console;
-    }
+    //    // from: https://visualstudio.microsoft.com/wp-content/uploads/2021/11/Home-page-extension-visual-updated.png
+    //    byte[] sampleImageData = File.ReadAllBytes(@"./samples/vsext.png");
 
-    [Fact]
-    public async Task FastCheckOCR()
-    {
-        FullOcrModel model = await OnlineFullModels.EnglishV3.DownloadAsync();
-
-        // from: https://visualstudio.microsoft.com/wp-content/uploads/2021/11/Home-page-extension-visual-updated.png
-        byte[] sampleImageData = File.ReadAllBytes(@"./samples/vsext.png");
-
-        using (PaddleOcrAll all = new(model)
-        {
-            AllowRotateDetection = true,
-            Enable180Classification = false,
-        })
-        {
-            // Load local file by following code:
-            // using (Mat src2 = Cv2.ImRead(@"C:\test.jpg"))
-            using (Mat src = Cv2.ImDecode(sampleImageData, ImreadModes.Color))
-            {
-                Stopwatch sw = Stopwatch.StartNew();
-                PaddleOcrResult result = all.Run(src);
-                _console.WriteLine($"lapsed={sw.ElapsedMilliseconds} ms");
-                _console.WriteLine("Detected all texts: \n" + result.Text);
-                foreach (PaddleOcrResultRegion region in result.Regions)
-                {
-                    _console.WriteLine($"Text: {region.Text}, Score: {region.Score}, RectCenter: {region.Rect.Center}, RectSize:    {region.Rect.Size}, Angle: {region.Rect.Angle}");
-                }
-            }
-        }
-    }
+    //    using (PaddleOcrAll all = new(model)
+    //    {
+    //        AllowRotateDetection = true,
+    //        Enable180Classification = false,
+    //    })
+    //    {
+    //        // Load local file by following code:
+    //        // using (Mat src2 = Cv2.ImRead(@"C:\test.jpg"))
+    //        using (Mat src = Cv2.ImDecode(sampleImageData, ImreadModes.Color))
+    //        {
+    //            Stopwatch sw = Stopwatch.StartNew();
+    //            PaddleOcrResult result = all.Run(src);
+    //            console.WriteLine($"lapsed={sw.ElapsedMilliseconds} ms");
+    //            console.WriteLine("Detected all texts: \n" + result.Text);
+    //            foreach (PaddleOcrResultRegion region in result.Regions)
+    //            {
+    //                console.WriteLine($"Text: {region.Text}, Score: {region.Score}, RectCenter: {region.Rect.Center}, RectSize:    {region.Rect.Size}, Angle: {region.Rect.Angle}");
+    //            }
+    //        }
+    //    }
+    //}
 
     [Fact]
     public async Task V4MkldnnRecTest()
@@ -57,70 +50,40 @@ public class OnlineModelsTest
         {
             Stopwatch sw = Stopwatch.StartNew();
             PaddleOcrRecognizerResult result = r.Run(src);
-            _console.WriteLine($"elapsed={sw.ElapsedMilliseconds}ms");
-            _console.WriteLine(result.ToString());
+            console.WriteLine($"elapsed={sw.ElapsedMilliseconds}ms");
+            console.WriteLine(result.ToString());
             Assert.True(result.Score > 0.9);
         }
     }
 
-    [Fact]
-    public async Task V4FastCheckOCR()
-    {
-        OnlineFullModels onlineModels = new OnlineFullModels(
-            OnlineDetectionModel.ChineseV4, OnlineClassificationModel.ChineseMobileV2, LocalDictOnlineRecognizationModel.EnglishV4);
-        FullOcrModel model = await onlineModels.DownloadAsync();
+    //[Fact]
+    //public async Task V4FastCheckOCR()
+    //{
+    //    OnlineFullModels onlineModels = new OnlineFullModels(
+    //        OnlineDetectionModel.ChineseV4, OnlineClassificationModel.ChineseMobileV2, LocalDictOnlineRecognizationModel.EnglishV4);
+    //    FullOcrModel model = await onlineModels.DownloadAsync();
 
-        // from: https://visualstudio.microsoft.com/wp-content/uploads/2021/11/Home-page-extension-visual-updated.png
-        byte[] sampleImageData = File.ReadAllBytes(@"./samples/vsext.png");
+    //    // from: https://visualstudio.microsoft.com/wp-content/uploads/2021/11/Home-page-extension-visual-updated.png
+    //    byte[] sampleImageData = File.ReadAllBytes(@"./samples/vsext.png");
 
-        using (PaddleOcrAll all = new(model)
-        {
-            AllowRotateDetection = true,
-            Enable180Classification = false,
-        })
-        {
-            // Load local file by following code:
-            // using (Mat src2 = Cv2.ImRead(@"C:\test.jpg"))
-            using (Mat src = Cv2.ImDecode(sampleImageData, ImreadModes.Color))
-            {
-                PaddleOcrResult result = null!;
-                Stopwatch sw = Stopwatch.StartNew();
-                result = all.Run(src);
-                sw.Stop();
+    //    using (PaddleOcrAll all = new(model)
+    //    {
+    //        AllowRotateDetection = true,
+    //        Enable180Classification = false,
+    //    })
+    //    {
+    //        // Load local file by following code:
+    //        // using (Mat src2 = Cv2.ImRead(@"C:\test.jpg"))
+    //        using (Mat src = Cv2.ImDecode(sampleImageData, ImreadModes.Color))
+    //        {
+    //            PaddleOcrResult result = null!;
+    //            Stopwatch sw = Stopwatch.StartNew();
+    //            result = all.Run(src);
+    //            sw.Stop();
 
-                _console.WriteLine($"elapsed={sw.ElapsedMilliseconds}ms");
-                _console.WriteLine("Detected all texts: \n" + result.Text);
-            }
-        }
-    }
-
-    [Fact(Skip = "Too slow")]
-    public async Task V4ServerTest()
-    {
-        OnlineFullModels onlineModels = OnlineFullModels.ChineseServerV4;
-        FullOcrModel model = await onlineModels.DownloadAsync();
-
-        // from: https://visualstudio.microsoft.com/wp-content/uploads/2021/11/Home-page-extension-visual-updated.png
-        byte[] sampleImageData = File.ReadAllBytes(@"./samples/vsext.png");
-
-        using (PaddleOcrAll all = new(model)
-        {
-            AllowRotateDetection = true,
-            Enable180Classification = false,
-        })
-        {
-            // Load local file by following code:
-            // using (Mat src2 = Cv2.ImRead(@"C:\test.jpg"))
-            using (Mat src = Cv2.ImDecode(sampleImageData, ImreadModes.Color))
-            {
-                PaddleOcrResult result = null!;
-                Stopwatch sw = Stopwatch.StartNew();
-                result = all.Run(src);
-                sw.Stop();
-
-                _console.WriteLine($"elapsed={sw.ElapsedMilliseconds}ms");
-                _console.WriteLine("Detected all texts: \n" + result.Text);
-            }
-        }
-    }
+    //            _console.WriteLine($"elapsed={sw.ElapsedMilliseconds}ms");
+    //            _console.WriteLine("Detected all texts: \n" + result.Text);
+    //        }
+    //    }
+    //}
 }
